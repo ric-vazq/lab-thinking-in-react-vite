a368ebbafc4f4f19a2bd162ed9ad2541ef0f17c4
Ricardo Vazquez <rickyrabv@gmail.com>
2024-02-08 16:20:46 -0300

    Cleaned Up Code

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -6,20 +6,28 @@ function ProductTable(props) {
     <table className="w-full text-base text-center">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
-          <th className="px-6 py-3" scope="col">Name</th>
-          <th className="px-6 py-3" scope="col">Price</th>
+          <th className="px-6 py-3" scope="col">
+            Name
+          </th>
+          <th className="px-6 py-3" scope="col">
+            Price
+          </th>
         </tr>
       </thead>
       <tbody>
-        {products.filter(product => {
+        {products
+          .filter((product) => {
             if (query === "") {
-                return product;
-            } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
-                return product; 
-            } else return; 
-        }).map((product) => {
-          return <ProductRow product={product} key={product.id} />;
-        })}
+              return product;
+            } else if (
+              product.name.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return product;
+            } else return;
+          })
+          .map((product) => {
+            return <ProductRow product={product} key={product.id} />;
+          })}
       </tbody>
     </table>
   );
